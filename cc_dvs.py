0254965bacc0cbdcbef984878d6a3a2d74bb1548
Fadion96 <nowak196@gmail.com>
2020-06-09 14:35:19 +0200

    Added scripts for cryptocharm library

---
+++ cc_dvs.py
@@ -0,0 +1,72 @@
+from charm.toolbox.pairinggroup import PairingGroup, ZR, G1, G2, GT, pair
+from functools import reduce
+import operator
+
+group = PairingGroup("SS512")
+
+def keygen(g):
+    sk_p, sk_v = group.random(ZR), group.random(ZR)
+    pk_p = g ** sk_p
+    pk_v = g ** sk_v
+    return sk_p, pk_p, sk_v, pk_v
+
+def sign(g, msg, pks, sk, p_id, v_id):
+    a_list = []
+    r_list = []
+    h_list = []
+    for i in range(len(pks)):
+        if i == p_id:
+            a_list.append(None)
+            r_list.append(None)
+            h_list.append(None)
+        else:
+            a_i = group.random(ZR)
+            a_list.append(a_i)
+            r_i = g ** a_i
+            r_list.append(r_i)
+            h_i = group.hash(f"{msg}{r_i}", ZR)
+            h_list.append(h_i)
+    a = group.random(ZR)
+    r_list[p_id] = (g ** a) * reduce(operator.mul, [pks[i] ** (- h_list[i]) for i in range(len(pks)) if i != p_id ])
+    h = group.hash(f"{msg}{r_list[p_id]}", ZR)
+    s = reduce(operator.add, [a_list[i] + a + (sk * h) for i in range(len(pks)) if i != p_id])
+    r_list[v_id] = pks[v_id] * a_list[v_id]
+    return (r_list, s)
+
+def verify(g, msg, pks, sk, p_id, v_id, R, s):
+    R[v_id] = R[v_id] ** (1/sk)
+    h_list = [group.hash(f"{msg}{R[i]}") for i in range(len(pks))]
+    prod = reduce(operator.mul, [R[i] * (pks[i] ** h_list[i]) for i in range(len(pks))])
+    assert g ** s == prod
+
+def sim(g, msg, pks, sk, p_id, v_id):
+    a_list = []
+    r_list = []
+    h_list = []
+    for i in range(len(pks)):
+        if i == v_id:
+            a_list.append(None)
+            r_list.append(None)
+            h_list.append(None)
+        else:
+            a_i = group.random(ZR)
+            a_list.append(a_i)
+            r_i = g ** a_i
+            r_list.append(r_i)
+            h_i = group.hash(f"{msg}{r_i}", ZR)
+            h_list.append(h_i)
+    a = group.random(ZR)
+    r = (g ** a) * reduce(operator.mul, [pks[i] ** (-h_list[i])for i in range(len(pks)) if i != v_id ])
+    h = group.hash(f"{msg}{r}", ZR)
+    s = reduce(operator.add, [a_list[i] + a + (sk * h) for i in range(len(pks)) if i != v_id])
+    r_list[v_id] = r ** sk
+    return (r_list, s)
+ 
+if __name__ == "__main__":
+    msg = "Master Thesis"
+    g = group.random(G1)
+    sk_p, pk_p, sk_v, pk_v = keygen(g)
+
+    # R, s = sign(g, msg, [pk_p, pk_v], sk_p, 0, 1)
+    R, s = sim(g, msg, [pk_p, pk_v], sk_v, 0, 1)
+    verify(g, msg, [pk_p, pk_v], sk_v, 0, 1, R, s)
